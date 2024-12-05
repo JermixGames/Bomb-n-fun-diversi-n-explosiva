fb70fdbfb97b7bfdf4bcf557e585f6c976ba9d96
RyqueGG <93305482+RyqueGG@users.noreply.github.com>
2024-12-05 17:43:30 -0500

    BOLA ROTACION CON IMPULSO AÑDIDA.
    Le tuve que poner un rigidbody a los stage con kinematic para que unity no me llorara y se congelara cuando la bola colisionaba con el stage.

--- Assets/Scenes/Enrique cosas/Scripts Enrique/Rotatenopivote.cs
+++ Assets/Scenes/Enrique cosas/Scripts Enrique/Rotatenopivote.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
 
-public class Rotatenopivote : MonoBehaviour
+public class RotateAndMovePlatform : MonoBehaviour
 {
+    public float rotationSpeed; 
+    public Transform platform; 
+    public Vector3 direction; 
 
-    public float rotationSpeed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.deltaTime); 
+        
+        transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.deltaTime);
+
+        
+        if (platform != null)
+        {
+            platform.GetComponent<Rigidbody>().MovePosition(platform.position + direction * Time.deltaTime);
+        }
     }
 }
