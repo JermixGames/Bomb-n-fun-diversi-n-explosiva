c068374a902ff33547fe1beba283d9d644f7181e
RyqueGG <93305482+RyqueGG@users.noreply.github.com>
2024-12-05 16:48:23 -0500

    Bola que rota en el stage añadida

---
+++ Assets/Scenes/Enrique cosas/Scripts Enrique/Rotate.cs
@@ -0,0 +1,19 @@
+//using UnityEngine;
+
+//public class Rotate : MonoBehaviour
+//{
+    //public float rotationSpeed;
+    //public GameObject pivotObject;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        //transform.RotateAround(pivotObject.transform.position, new Vector3(0, 1, 0), rotationSpeed * Time.deltaTime);
+        
+   // }
+//}
